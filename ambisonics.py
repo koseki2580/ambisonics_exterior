41c40a21403309ea87ea149dae497e6aa2dc8f38
koseki2580 <kingdom_hearts2580@yahoo.co.jp>
2020-05-20 14:42:19 +0900

    update

--- ambisonics.py
+++ ambisonics.py
@@ -8,7 +8,7 @@ import matplotlib.cm as cm
 #初期条件
 ambi_order = 7              #打ち切り次数
 freq = 550                  #周波数
-pos_s = np.array([0.0001,0,0])   #一次音源の座標x,y,z
+pos_s = np.array([0.1,0,0])   #一次音源の座標x,y,z
 cv = 344                    #音速
 r_sp = 0.5                  #スピーカーを配置する半径
 r_mic = 1.0                #マイクロホンの半径
@@ -65,12 +65,9 @@ for i in range(X.size):
     for j in range(Y.size):
         NE[i,j] = 10*np.log10(((np.abs(P_HOA[i,j]-P_org[i,j]))**2)/((np.abs(P_org[i,j]))**2))
 
-#スイートスポット
-rad = (ambi_order*cv)/(2*np.pi*freq)
 
 #グラフ
 #所望音場
-sweet_spot = patches.Circle(xy=(0, 0), radius=rad, ec='r',fill=False)
 r_sp_ = patches.Circle(xy=(0, 0), radius=r_sp, ec='b',fill=False)
 plt.figure()
 ax = plt.axes()
@@ -78,7 +75,6 @@ im = ax.imshow(np.real(P_org), interpolation='gaussian',cmap=cm.jet,origin='lowe
                extent=[-Plot_range, Plot_range, -Plot_range, Plot_range],
                vmax=0.1, vmin=-0.1)
 ax.grid(False)
-ax.add_patch(sweet_spot)
 ax.add_patch(r_sp_)
 plt.axis('scaled')
 ax.set_aspect('equal')
@@ -88,7 +84,6 @@ plt.savefig('original_sound.pdf',bbox_inches="tight",dpi = 64,
 plt.show()
 
 #再現音場
-sweet_spot = patches.Circle(xy=(0, 0), radius=rad, ec='r',fill=False)
 r_sp_ = patches.Circle(xy=(0, 0), radius=r_sp, ec='b',fill=False)
 plt.figure()
 ax2 = plt.axes()
@@ -96,7 +91,6 @@ im2 = ax2.imshow(np.real(P_HOA), interpolation='gaussian',cmap=cm.jet,origin='lo
                extent=[-Plot_range, Plot_range, -Plot_range, Plot_range],
                vmax=0.1, vmin=-0.1)
 ax2.grid(False)
-ax2.add_patch(sweet_spot)
 ax2.add_patch(r_sp_)
 plt.axis('scaled')
 ax2.set_aspect('equal')
@@ -106,7 +100,6 @@ plt.savefig('reproduced_sound.pdf',bbox_inches="tight",dpi = 64,
 plt.show()
 
 #正規化誤差
-sweet_spot = patches.Circle(xy=(0, 0), radius=rad, ec='r',fill=False)
 r_sp_ = patches.Circle(xy=(0, 0), radius=r_sp, ec='b',fill=False)
 plt.figure()
 ax3 = plt.axes()
@@ -114,7 +107,6 @@ im3 = ax3.imshow(np.real(NE), interpolation='gaussian',cmap=cm.pink_r,origin='lo
                extent=[-Plot_range, Plot_range, -Plot_range, Plot_range],
                vmax=0, vmin=-60)
 ax3.grid(False)
-ax3.add_patch(sweet_spot)
 ax3.add_patch(r_sp_)
 plt.axis('scaled')
 ax3.set_aspect('equal')
